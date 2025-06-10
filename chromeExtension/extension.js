dc85ab5538cc796aab5ab53f31638c154dbf47e0
Manas Ranjan <manasrajan9852@gmai.com>
2025-06-10 16:19:57 +0530

    Added basic feature

--- chromeExtension/extension.js
+++ chromeExtension/extension.js
@@ -1,5 +1,19 @@
-let saveBtn = document.getElementById("input-btn");
+const saveBtn = document.getElementById("input-btn");
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
 saveBtn.addEventListener("click", () => {
-    console.log("Button Clicked")
+   let inputValue = inputEl.value;
+   myLeads.push(inputValue)
+   console.log(myLeads)
 });
+
+
+for (let  i = 0; i< myLeads.length ; i++){
+   // ulEl.innerHTML + = "<li>" + myLeads[i] + "</li>"
+
+   let li = document.createElement("li")
+   li.textContent = myLeads[i]
+   ulEl.append(li)
+}
--- toDo/todo.js
+++ toDo/todo.js
@@ -1,15 +1,67 @@
-let inputSlider = document.getElementById("inputSlider")
-let sliderValue = document.getElementById("sliderValue")
-let sliderValue = document.getElementById("sliderValue")
+const input = document.querySelector("input");
+const addBtn = document.querySelector("#add-btn");
+const list = document.querySelector("ul");
 
-let sliderValue = document.getElementById("sliderValue")
-let sliderValue = document.getElementById("sliderValue")
-let sliderValue = document.getElementById("sliderValue")
-let sliderValue = document.getElementById("sliderValue")
+let tasks = []
 
 
-sliderValue.textContent = inputSlider.value
-
-inputSlider.addEventListener('input', ()=>{
-    sliderValue.textContent = inputSlider.value
+//Even listenr for addition of task 
+addBtn.addEventListener("click", addTask)
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTask();
+    }
 })
+
+function addTask() {
+    const inputText = input.value.trim();
+
+    if (!inputText) return;
+
+    const task = {
+        id: Date.now(),
+        text: inputText
+    };
+
+    tasks.push(task);
+    saveTasks();
+    renderTask(task);
+    input.value = "";
+
+}
+function renderTask(task){
+const li = document.createElement("li");
+const span = document.createElement("span");
+const deleteBtn = document.createElement("button");
+deleteBtn.classList.add("delete-btn");
+
+deleteBtn.textContent = "🗑️";
+
+span.textContent = task.text;
+
+
+deleteBtn.addEventListener("click", () => {
+    li.remove();
+    tasks = tasks.filter(t => t.id !== task.id);
+    saveTasks();
+});
+
+li.appendChild(span);
+li.appendChild(deleteBtn);
+list.appendChild(li);
+}
+
+function saveTasks(){
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+
+function loadTasks(){
+    const stored = localStorage.getItem("tasks");
+    if(stored){
+        tasks = JSON.parse(stored);
+        tasks.forEach(renderTask);
+    }
+}
+
+loadTasks()
